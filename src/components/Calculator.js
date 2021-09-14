dd747644d758d1dfc7f63c7ff67b5a2ffe099232
ridaarif98 <ridaarif16@gmail.com>
2021-09-14 12:05:12 +0200

    Create a directory and add a file Calculator.js

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class CalculatorDesign extends React.Component {
+  render() {
+    return (
+      <ul>
+          <li>0</li>
+      </ul>
+    );
+  }
+}
+
+export default CalculatorDesign;
