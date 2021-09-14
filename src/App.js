ecb501452567ae614ac8c368ffcbe032de81052f
ridaarif98 <ridaarif16@gmail.com>
2021-09-14 13:07:46 +0200

    Create a class in Claculator.js to render calculator elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
+import './styles/style.css'
 import CalculatorDesign from "./components/Calculator.js";
 
 function App() {
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,54 @@
 import React from 'react';
+// import styles from '../styles/style.css'
+
 class CalculatorDesign extends React.Component {
   render() {
     return (
-      <ul>
-          <li>0</li>
+      <ul className="mainList">
+        <li>
+          <ul className="resultList">
+              <li>Result</li>
+          </ul>
+        </li>
+        <li>
+          <ul className="buttonList">
+              <li>AC</li>
+              <li>+/-</li>
+              <li>%</li>
+              <li>÷</li>
+          </ul>
+        </li>
+        <li>
+          <ul className="buttonList">
+              <li>7</li>
+              <li>8</li>
+              <li>9</li>
+              <li>x</li>
+          </ul>
+        </li>
+        <li>
+          <ul className="buttonList">
+              <li>4</li>
+              <li>5</li>
+              <li>6</li>
+              <li>-</li>
+          </ul>
+        </li>
+        <li>
+          <ul className="buttonList">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>+</li>
+          </ul>
+        </li>
+        <li>
+          <ul className="buttonListBottom">
+              <li>0</li>
+              <li>.</li>
+              <li>=</li>
+          </ul>
+        </li>
       </ul>
     );
   }
