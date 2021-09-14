85f9169fad000236064bdd953e91965a69eb5146
ridaarif98 <ridaarif16@gmail.com>
2021-09-14 16:40:04 +0200

    Replace App function with class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './styles/style.css';
 import CalculatorDesign from './components/Calculator';
+import React from 'react';
 
-function App() {
-  return (
-    <CalculatorDesign />
-  );
+// function App() {
+//   return (
+//     <CalculatorDesign />
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <CalculatorDesign />
+    );
+  }
 }
 
 export default App;
