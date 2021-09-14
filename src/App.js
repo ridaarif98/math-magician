e33a66f86bfca98dfe77c7bae18e35f5d714a665
ridaarif98 <ridaarif16@gmail.com>
2021-09-14 16:46:23 +0200

    Add constructor in App class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import React from 'react';
 import './styles/style.css';
 import CalculatorDesign from './components/Calculator';
-import React from 'react';
-
-// function App() {
-//   return (
-//     <CalculatorDesign />
-//   );
-// }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { res: '' };
+  }
+
   render() {
+    const { res } = this.state;
     return (
+      <div>{ res }
       <CalculatorDesign />
+      </div>
     );
   }
 }
