05fd41c4d3ad0fceadee18686234d13d786187cc
ridaarif98 <ridaarif16@gmail.com>
2021-09-15 16:21:42 +0200

    Fix some linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,11 +20,6 @@ class CalculatorDesign extends React.Component {
   }
 
   render() {
-    const opArray = ['AC', '+/-', '%', '÷'].map((test) => <li><button type="button" className="calculateBtn" name={test} key={test} value={test} onClick={this.handleChange}>{test}</button></li>);
-    const numArrayOne = [7, 8, 9, 'x'].map((test) => <li><button type="button" className="calculateBtn" name={test}  key={test} value={test} onClick={this.handleChange}>{test}</button></li>);
-    const numArrayTwo = [4, 5, 6, '-'].map((test) => <li><button type="button" className="calculateBtn" name={test}  key={test} value={test} onClick={this.handleChange}>{test}</button></li>);
-    const numArrayThree = [1, 2, 3, '+'].map((test) => <li><button type="button" className="calculateBtn" name={test}  key={test} value={test} onClick={this.handleChange}>{test}</button></li>);
-    const numArrayFour = [0, '.', '='].map((test) => <li><button type="button" className="calculateBtn" name={test}   key={test} value={test} onClick={this.handleChange}>{test}</button></li>);
 
     return (
       <ul className="mainList">
@@ -39,27 +34,41 @@ class CalculatorDesign extends React.Component {
         </li>
         <li>
           <ul className="buttonList">
-            {opArray}
+          <li><button type="button" className="calculateBtn" name='AC' value='AC' onClick={this.handleChange}>AC</button></li>
+          <li><button type="button" className="calculateBtn" name='+/-' value='+/-' onClick={this.handleChange}>+/-</button></li>
+          <li><button type="button" className="calculateBtn" name='%'  value='%' onClick={this.handleChange}>%</button></li>
+          <li><button type="button" className="calculateBtn" name='÷' value='÷' onClick={this.handleChange}>÷</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonList">
-            {numArrayOne}
+          <li><button type="button" className="calculateBtn" name='7'  value='7' onClick={this.handleChange}>7</button></li>
+          <li><button type="button" className="calculateBtn" name='8'  value='8' onClick={this.handleChange}>8</button></li>
+          <li><button type="button" className="calculateBtn" name='9'  value='9' onClick={this.handleChange}>9</button></li>
+          <li><button type="button" className="calculateBtn" name='x'  value='x' onClick={this.handleChange}>x</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonList">
-            {numArrayTwo}
+          <li><button type="button" className="calculateBtn" name='4'  value='4' onClick={this.handleChange}>4</button></li>
+          <li><button type="button" className="calculateBtn" name='5'  value='5' onClick={this.handleChange}>5</button></li>
+          <li><button type="button" className="calculateBtn" name='6'  value='6' onClick={this.handleChange}>6</button></li>
+          <li><button type="button" className="calculateBtn" name='-'  value='-' onClick={this.handleChange}>-</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonList">
-            {numArrayThree}
+          <li><button type="button" className="calculateBtn" name='1'  value='1' onClick={this.handleChange}>1</button></li>
+          <li><button type="button" className="calculateBtn" name='2'  value='2' onClick={this.handleChange}>2</button></li>
+          <li><button type="button" className="calculateBtn" name='3'  value='3' onClick={this.handleChange}>3</button></li>
+          <li><button type="button" className="calculateBtn" name='+'  value='+' onClick={this.handleChange}>+</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonListBottom">
-            {numArrayFour}
+          <li><button type="button" className="calculateBtn" name='0'  value='0' onClick={this.handleChange}>0</button></li>
+          <li><button type="button" className="calculateBtn" name='.'  value='.' onClick={this.handleChange}>.</button></li>
+          <li><button type="button" className="calculateBtn" name='='  value='=' onClick={this.handleChange}>=</button></li>
           </ul>
         </li>
       </ul>
