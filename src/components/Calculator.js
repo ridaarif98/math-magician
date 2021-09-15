34d257303ef96db33120432365efb9c48fcc50f3
ridaarif98 <ridaarif16@gmail.com>
2021-09-15 13:11:16 +0200

    Add buttons assign them values, name and call event of them

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,19 @@
 import React from 'react';
+// import calculate from '../logic/calculator';
 
 class CalculatorDesign extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { result: 0 };
+    this.state = {
+      result: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log(this.state, e.target.name);
   }
 
   render() {
@@ -17,41 +27,41 @@ class CalculatorDesign extends React.Component {
         </li>
         <li>
           <ul className="buttonList">
-            <li>AC</li>
-            <li>+/-</li>
-            <li>%</li>
-            <li>÷</li>
+            <li><button className="calculateBtn" name="AC" value="AC" onClick={this.handleChange}>AC</button></li>
+            <li><button className="calculateBtn" name="+/-" value="+/-" onClick={this.handleChange}>+/-</button></li>
+            <li><button className="calculateBtn" name="%" value="%" onClick={this.handleChange}>%</button></li>
+            <li><button className="calculateBtn" name="÷" value="÷" onClick={this.handleChange}>÷</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonList">
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>x</li>
+            <li><button className="calculateBtn" name='7' value="7" onClick={this.handleChange}>7</button></li>
+            <li><button className="calculateBtn" name="8" value="8" onClick={this.handleChange}>8</button></li>
+            <li><button className="calculateBtn" name="9" value="9" onClick={this.handleChange}>9</button></li>
+            <li><button className="calculateBtn" name="x" value="x" onClick={this.handleChange}>x</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonList">
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>-</li>
+            <li><button className="calculateBtn" name="4" value="4" onClick={this.handleChange}>4</button></li>
+            <li><button className="calculateBtn" name="5" value="5" onClick={this.handleChange}>5</button></li>
+            <li><button className="calculateBtn" name="6" value="6" onClick={this.handleChange}>6</button></li>
+            <li><button className="calculateBtn" name="-" value="-" onClick={this.handleChange}>-</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonList">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>+</li>
+            <li><button className="calculateBtn" name="1" value="1" onClick={this.handleChange}>1</button></li>
+            <li><button className="calculateBtn" name="2" value="2" onClick={this.handleChange}>2</button></li>
+            <li><button className="calculateBtn" name="3" value="3" onClick={this.handleChange}>3</button></li>
+            <li><button className="calculateBtn" name="+" value="+" onClick={this.handleChange}>+</button></li>
           </ul>
         </li>
         <li>
           <ul className="buttonListBottom">
-            <li>0</li>
-            <li>.</li>
-            <li>=</li>
+            <li><button className="calculateBtn" name="0" value="0" onClick={this.handleChange}>0</button></li>
+            <li><button className="calculateBtn" name="." value="." onClick={this.handleChange}>.</button></li>
+            <li><button className="calculateBtn" name="=" value="=" onClick={this.handleChange}>=</button></li>
           </ul>
         </li>
       </ul>
