03cf06bcccd20e363535d0db33db11c89d8528e0
ridaarif98 <ridaarif16@gmail.com>
2021-09-16 15:34:53 +0200

    Use map to display buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -28,33 +28,22 @@ function CalculatorDesign() {
       </li>
       <li>
         <ul className="buttonList">
-          <li><button type="button" className="calculateBtn" name="7" value="7" onClick={handleChange}>7</button></li>
-          <li><button type="button" className="calculateBtn" name="8" value="8" onClick={handleChange}>8</button></li>
-          <li><button type="button" className="calculateBtn" name="9" value="9" onClick={handleChange}>9</button></li>
-          <li><button type="button" className="calculateBtn" name="x" value="x" onClick={handleChange}>x</button></li>
+          {['7', '8', '9', 'x'].map((btn) => (<li key={btn}><button type="button" className="calculateBtn" name={btn} value={btn} onClick={handleChange}>{btn}</button></li>))}
         </ul>
       </li>
       <li>
         <ul className="buttonList">
-          <li><button type="button" className="calculateBtn" name="4" value="4" onClick={handleChange}>4</button></li>
-          <li><button type="button" className="calculateBtn" name="5" value="5" onClick={handleChange}>5</button></li>
-          <li><button type="button" className="calculateBtn" name="6" value="6" onClick={handleChange}>6</button></li>
-          <li><button type="button" className="calculateBtn" name="-" value="-" onClick={handleChange}>-</button></li>
+          {['4', '5', '6', '-'].map((btn) => (<li key={btn}><button type="button" className="calculateBtn" name={btn} value={btn} onClick={handleChange}>{btn}</button></li>))}
         </ul>
       </li>
       <li>
         <ul className="buttonList">
-          <li><button type="button" className="calculateBtn" name="1" value="1" onClick={handleChange}>1</button></li>
-          <li><button type="button" className="calculateBtn" name="2" value="2" onClick={handleChange}>2</button></li>
-          <li><button type="button" className="calculateBtn" name="3" value="3" onClick={handleChange}>3</button></li>
-          <li><button type="button" className="calculateBtn" name="+" value="+" onClick={handleChange}>+</button></li>
+          {['1', '2', '3', '+'].map((btn) => (<li key={btn}><button type="button" className="calculateBtn" name={btn} value={btn} onClick={handleChange}>{btn}</button></li>))}
         </ul>
       </li>
       <li>
         <ul className="buttonListBottom">
-          <li><button type="button" className="calculateBtn" name="0" value="0" onClick={handleChange}>0</button></li>
-          <li><button type="button" className="calculateBtn" name="." value="." onClick={handleChange}>.</button></li>
-          <li><button type="button" className="calculateBtn" name="=" value="=" onClick={handleChange}>=</button></li>
+          {['0', '.', '='].map((btn) => (<li key={btn}><button type="button" className="calculateBtn" name={btn} value={btn} onClick={handleChange}>{btn}</button></li>))}
         </ul>
       </li>
     </ul>
